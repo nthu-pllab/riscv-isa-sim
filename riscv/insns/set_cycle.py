8e26eb88e6a12eea2cdb889d6667c254f0451f94
chchang <ookk8282@gmail.com>
2020-06-29 15:26:44 +0800

    Script to add insn/cycle count macro into all insn

---
+++ riscv/insns/set_cycle.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python3
+
+from pathlib import Path
+
+INSN_DIR = Path(__file__).resolve().parents[0]
+
+INSN_COUNT_MACRO = "ADD_INSN_COUNT"
+CYCLE_COUNT_MACRO = "ADD_CYCLE_COUNT"
+
+
+def is_vext_insn(filename: Path):
+    return filename.name.startswith("v")
+
+
+def is_header_file(filename: Path):
+    return filename.suffix == ".h"
+
+
+def add_ic_macro(filename: Path):
+    content = ""
+    with filename.open("r") as f:
+        for line in f:
+            if INSN_COUNT_MACRO in line:
+                continue
+            if CYCLE_COUNT_MACRO in line:
+                continue
+            content += line
+    #  cycle = 1
+    #  if is_vext_insn(filename):
+    #      cycle = 3
+    #  content += f"{INSN_COUNT_MACRO}(1);\n"
+    #  content += f"{CYCLE_COUNT_MACRO}({cycle});\n"
+    with filename.open("w") as f:
+        f.write(content)
+
+
+if __name__ == "__main__":
+    for filename in INSN_DIR.iterdir():
+        if not is_header_file(filename):
+            continue
+        add_ic_macro(filename)
